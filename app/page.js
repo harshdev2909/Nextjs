81ad52d8a24c1abf83ac4ead76662084a06aa3e6
harshdev2909 <harsh.dev2909@gmail.com>
2024-11-21 19:11:23 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -7,9 +7,9 @@ export default function Home() {
 
   }
   return (
-   <div>
+   <div className="">
     <p className="text-center font-bold">Hello</p>
-    <Link href="/login">Dashboard</Link>
+    <Link className="bg-white text-black rounded-lg w-full h-full border text-sm m-20 ml-20 justify-center items-center" href="/login">Dashboard</Link>
     {/* <button className="bg-white text-black m-10xl items-center rounded-md w-15px h-10px">Hello</button> */}
    </div>
   );
